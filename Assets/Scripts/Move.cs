85e6904b49aa960395b4bcdab7a2bb32343ab612
Lirian Su <liriansu@gmail.com>
2017-02-10 23:33:31 +0800

    use c sharp

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+    public float OutX = -5.0f;
+    public float OutY = 3.0f;
+    public int Count = 1;
+
+    private void OnMouseDown() {
+        print("clicked in C Sharp");
+        var position = new Vector3(OutX, OutY);
+        var rotation = new Quaternion(0, 0, 0, 0);
+        var otherCube = Instantiate(gameObject, position, rotation);
+        otherCube.name = string.Format("Card%s", Count);
+        OutX += 1;
+        if (OutX > 5.1) {
+            OutX = -5;
+            OutY -= 1.1f;
+        }
+    }
+}
