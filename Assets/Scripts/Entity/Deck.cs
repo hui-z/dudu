bfdceba765d67df1893fdecebfda3d7ab2af2871
Lirian Su <liriansu@gmail.com>
2017-02-11 13:18:26 +0800

    add deck/card

---
+++ Assets/Scripts/Entity/Deck.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Entity {
+    public class Deck {
+        private int _head;
+        private List<Card> _cards;
+
+        private void InitializeCards() {
+            _cards = new List<Card>(136);
+            for (var suit = 1; suit <= 3; suit++) {
+                for (var size = 1; size <= 9; size++) {
+                    for (var seq = 1; seq <= 4; seq++) {
+                        _cards.Add(new Card(suit, size, seq));
+                    }
+                }
+            }
+            for (var size = 1; size <= 7; size++) {
+                for (var seq = 1; seq <= 4; seq++) {
+                    _cards.Add(new Card(4, size, seq));
+                }
+            }
+            // TODO randomize _cards
+        }
+
+        public Deck() {
+            _head = 0;
+            InitializeCards();
+        }
+
+        public int GetSize() {
+            return _cards.Count() - _head;
+        }
+
+        public Card Draw() {
+            return _cards[_head++];
+        }
+    }
+}
--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,16 +1,26 @@
-﻿using UnityEngine;
+﻿using Entity;
+using UnityEngine;
 
 public class Move : MonoBehaviour {
     public float OutX = -5.0f;
     public float OutY = 3.0f;
-    public int Count = 1;
+    public int Count = 0;
+    private Deck _deck;
+
+    private void Start() {
+        _deck = new Deck();
+    }
 
     private void OnMouseDown() {
         print("clicked in C Sharp");
+        if (_deck.GetSize() > 0) {
+            print(_deck.GetSize());
+            print(_deck.Draw().ToString());
+        }
         var position = new Vector3(OutX, OutY);
         var rotation = new Quaternion(0, 0, 0, 0);
         var otherCube = Instantiate(gameObject, position, rotation);
-        otherCube.name = string.Format("Card {0}", Count);
+        otherCube.name = string.Format("Card {0}", ++Count);
         OutX += 1;
         if (OutX > 5.1) {
             OutX = -5;
